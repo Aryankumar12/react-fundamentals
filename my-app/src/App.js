bc5ed3448ef338a549f3e19a4939f87015de0ec0
Aryan Kumer <aryandot1@gmail.com>
2026-01-16 15:20:22 +0530

    usestate explained

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,17 @@
-import Card from './Card';
-import Footer from './Footer';
-import Greeting from './Greeting';
-import Header from './Header';
+import React from 'react'
+import { useState } from 'react'
 
-import React from 'react';
-import { UserContext } from './context/UserContext';
-function App() {
+const App = () => {
 
-  const users = [
-    { id: 1, firstName: "Aryan", lastName: "Kumar" },
-    { id: 2, firstName: "Rahul", lastName: "Sharma" }
-  ];
-
+  const [count, setCount]= useState(0);
   return (
+    <>
 
-  <UserContext.Provider value ={users}>
-
-
-<Header/>
-<Greeting/>
-<Card />
-<Card />
-
-<Footer/>
-  </UserContext.Provider>
+      <h1>Count: {count} </h1>
+      <button onClick={()=> setCount(count+1)}>Increment</button>
+      <button onClick={()=> setCount(count-1)}>Decrement</button>
+    </>
   )
-
 }
 
-
-export default App;
+export default App
