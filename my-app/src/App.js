7a9c8a6d46a48a94b59e7444d0f2b797800500b3
Aryan Kumer <aryandot1@gmail.com>
2026-01-16 15:24:50 +0530

    forms handled

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useState } from 'react'
-
 const App = () => {
 
-  const [count, setCount]= useState(0);
-  return (
-    <>
 
-      <h1>Count: {count} </h1>
-      <button onClick={()=> setCount(count+1)}>Increment</button>
-      <button onClick={()=> setCount(count-1)}>Decrement</button>
-    </>
+  const [name, setName] = useState('')
+
+  const handleClick = ()=>{
+    alert(`Your name is ${name}`)
+  }
+  return (
+   <>
+    <h2>Your Name : {name}</h2>
+    <input type='text' value={name} onChange={(e)=> setName(e.target.value) }/>
+    <button onClick={handleClick}>Click Me</button>
+   </>
   )
 }
 
